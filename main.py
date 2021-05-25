2dd86fbd1ac4aeaf0e2ea60a0f584758330004b6
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-05-25 16:38:27 +0530

    basic dadbot functionality

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to discord!')
 
+@client.event
+async def on_message(message):
+    if (message.content.lower().startswith('im')):
+        res = "Hi " + message.content[3:] + ", I'm nitrawf-bot"
+        await message.channel.send(res)
+
 client.run(TOKEN)
