b53c4f7344a6994d15d6974437401bbacdeb1cad
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-05-25 18:35:57 +0530

    set up basic api routes

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 import discord
 from dotenv import load_dotenv
 from api import API
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 API_URL = os.getenv('API_URL')
+OSU_API = os.getenv('OSU_API_KEY')
 
 client = discord.Client()
-api = API(API_URL)
+api = API(API_URL, OSU_API)
 
 @client.event
 async def on_ready():
